3dc5528839bff5cf8837be7423d1b1b64c053a3c
Irlitsa Dmitry <irlitsa.d.p@gmail.com>
2022-10-12 19:04:28 +0300

    Initialize project

---
+++ sale_notebook.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+
+def get_html(url):
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
+        "accept": "* / *"
+    }
+
+    r = requests.get(url, headers=headers, timeout=5)
+    # with open("sale_notebook.html", "wb") as f:
+    #     f.write(r.content)
+
+    # with open("sale_notebook.html", "rb") as f:
+    #     html_content = f.read()
+
+    notebooks = []
+    # soup = BeautifulSoup(html_content, "lxml")
+    soup = BeautifulSoup(r.content, "lxml")
+    goods = soup.find_all("tr", class_="hide-mob")
+    for item in goods:
+        title = item.td.a.string.replace("восстановленный ", "")
+        link = "https://www.notik.ru" + item.td.a["href"]
+        series = item.td.find("b", class_="wordwrap").string
+        vendor_code = item.td.find("div", class_="artikul").string
+        
+        characteristics = item.findNext("tr")
+        price = characteristics.find("a", class_="tocart")["ecprice"]
+        description = characteristics.find("a", class_="tocart")["ecname"]
+
+        notebooks.append(
+            {
+                "Title": title,
+                "Description": description,
+                "Link": link,
+                "Series": series,
+                "Vendor code": vendor_code,
+                "Price": price
+            }
+        )
+        # break
+    with open("notebooks.json", "w", encoding="utf-8") as file:
+        json.dump(notebooks, file, indent=4, ensure_ascii=False)
+    print("scraping finished")
+
+
+def main():
+    # sites scraping:
+    # https://www.notik.ru/
+    # https://www.alt-del.ru/
+    get_html("https://www.notik.ru/search_catalog/filter/sales.htm?sortby=price") # - all models
+
+if __name__ == '__main__':
+    main()
